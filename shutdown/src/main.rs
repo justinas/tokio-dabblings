8a0b9d06f44787a00554daee726449d4b6fc3606
Justinas Stankevicius <justinas@justinas.org>
2017-01-17 20:59:26 +0200

    Add EchoShutdownSink

--- shutdown/src/main.rs
+++ shutdown/src/main.rs
@@ -1,10 +1,49 @@
 extern crate futures;
 extern crate tokio_core;
 
-use futures::{Future, Stream};
-use tokio_core::io::{copy, Io};
-use tokio_core::net::TcpListener;
+use futures::{AsyncSink, Future, Poll, Sink, StartSend, Stream};
+use futures::stream::{MergedItem, SplitSink};
+use futures::sync::mpsc;
+use tokio_core::io::{copy, Framed, Io};
+use tokio_core::net::{TcpListener, TcpStream};
 use tokio_core::reactor::Core;
+
+struct ShutdownRequest;
+
+/// Implements a stream sink which:
+///     * Sends a ShutdownRequest to the given channel if a "shutdown" message is sent to this sink
+///     * Forwards any other message to the underlying sink
+struct EchoShutdownSink {
+    channel: mpsc::Sender<ShutdownRequest>,
+    client_sink: SplitSink<Framed<TcpStream, codec::LineCodec>>,
+    handle: tokio_core::reactor::Handle,
+}
+
+impl Sink for EchoShutdownSink {
+    type SinkItem = String;
+    type SinkError = ();
+
+    fn start_send(&mut self, item: String) -> StartSend<Self::SinkItem, Self::SinkError> {
+        println!("{:?}", item);
+        match item {
+            ref i if i == "shutdown" => {
+                self.handle.spawn(self.channel
+                    .clone()
+                    .send(ShutdownRequest)
+                    .map(|_| ())
+                    .map_err(|_| ()));
+                Ok(AsyncSink::Ready)
+            }
+            i => self.client_sink.start_send(i).map_err(|_| ()),
+        }
+    }
+    fn poll_complete(&mut self) -> Poll<(), Self::SinkError> {
+        self.channel
+            .poll_complete()
+            .map_err(|_| ())
+            .and_then(|_| self.client_sink.poll_complete().map_err(|_| ()))
+    }
+}
 
 fn main() {
     let mut core = Core::new().expect("Could not crate the event loop");
@@ -13,13 +52,27 @@ fn main() {
     let addr = "127.0.0.1:7000".parse().unwrap();
     let listener = TcpListener::bind(&addr, &handle).expect("Could not bind the socket");
 
-    let server = listener.incoming().for_each(|(client, addr)| {
-        let (reader, writer) = client.split();
+    let (sender, receiver) = mpsc::channel(0);
+    let server = listener.incoming()
+        .map_err(|_| ()) // TODO: do not discard the I/O error
+        .merge(receiver).for_each(|thing| {
+        match thing {
+            MergedItem::First((socket, addr)) => {
+                let (client_sink, client_stream) = socket.framed(codec::LineCodec).split();
+                let future = EchoShutdownSink {
+                    channel: sender.clone(),
+                    client_sink: client_sink,
+                    handle: handle.clone(),
+                }.send_all(client_stream.map_err(|_| ()));
+                handle.spawn(future.map(|_| ()));
+            }
 
-        let future = copy(reader, writer)
-            .map(move |amt| println!("wrote {} bytes to {}", amt, addr))
-            .map_err(|err| println!("IO error: {:?}", err));
-        handle.spawn(future);
+            // for_each breaks the "loop" when it receives an errored future from the closure
+            // impl<T, E> IntoFuture for Result<T, E> helps us here as well.
+            MergedItem::Second(_) => return Err(()),
+            MergedItem::Both(_, _) => return Err(()),
+        }
+
         Ok(())
     });
     println!("Listening on localhost:7000");
